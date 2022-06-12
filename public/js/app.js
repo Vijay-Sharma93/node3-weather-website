0fc0a1aaaa6cc690e3cb4d7384a5198b2d153ae2
Your Name <you@example.com>
2022-06-12 14:55:23 +0530

    removed logs

---
+++ public/js/app.js
@@ -0,0 +1,20 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((forcastData) => {
+            if (forcastData.error) {
+                messageOne.textContent = forcastData.error
+            } else {
+                messageOne.textContent = forcastData.forecast
+            }
+        })
+    })
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+const forecast = (address, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query='+ address
+ 
+     request({ url: url, json: true }, (error, response) => {
+         if (error) {
+             callback('Unable to connect to weather service', undefined)
+         } else if (response.body.error) {
+             callback('Unable to find location', undefined)
+         } else {
+            console.log('Temporature '+response.body.current.temperature)
+             callback(undefined, {
+                temperature: response.body.current.temperature
+             })
+         }
+     })
+ }
+
+ module.exports = forecast
